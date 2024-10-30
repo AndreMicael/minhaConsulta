0618e8ac30c015f26366b5d55cf74afb28cb09b5
André Sampaio <49279300+AndreMicael@users.noreply.github.com>
2024-10-30 10:36:38 -0300

    feat: model despesa criado

---
+++ src/main/java/com/alg3/minhaconsulta/model/Despesa.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.alg3.minhaconsulta.model;
+import com.alg3.minhaconsulta.dao.ExceptionDAO;
+
+/**
+ *
+ * @author andresampaio
+ */
+public class Despesa {
+    private String descricao;
+    private String tipo;
+    private double valor;
+    private String dataRegistro;
+    
+    //Getters and setters
+    
+    public String getDescricao(){
+        return descricao;
+    }
+    
+    public String getTipo(){
+        return tipo;
+    }
+    
+    public double getValor(){
+        return valor;
+    }
+    
+    public String setDataRegistro(){
+        return dataRegistro;
+    }
+    
+     public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+     
+    public void setTipo(String tipo){
+        this.tipo = tipo;
+    }
+    
+    public void setValor(double valor){
+        this.valor = valor;
+    }
+    
+    public void setDataRegistro(String dataRegistro){
+        this.dataRegistro = dataRegistro;
+    }
+    
+     public void cadastrarDespesa(Despesa despesa) throws ExceptionDAO {
+        new DespesaDAO().cadastrarDespesa(despesa);
+    }
+}
